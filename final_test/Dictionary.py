51e2f7f8458d5a73e9de46ac90f7f255109d98b7
muhalwi79446-ai <muh.alwi79446@gmail.com>
2025-12-11 12:43:46 +0800

    Update Dictionary.py

--- final_test/Dictionary.py
+++ final_test/Dictionary.py
@@ -1,1 +1,48 @@
+# dictionary.py
+
+# Membuat dictionary
+mahasiswa = {
+    "nama": "Alwi",
+    "nim": "D0425007",
+    "jurusan": "sistem Informasi",
+    "nilai": 95
+}
+
+# Menampilkan dictionary
+print("Data Mahasiswa:")
+print(mahasiswa)
+
+# Mengakses nilai berdasarkan key
+print("\nNama:", mahasiswa["nama"])
+print("NIM :", mahasiswa["nim"])
+
+# Menambah key dan value baru
+mahasiswa["angkatan"] = 2023
+print("\nSetelah menambah angkatan:")
+print(mahasiswa)
+
+# Mengubah value di dictionary
+mahasiswa["nilai"] = 95
+print("\nSetelah mengubah nilai:")
+print(mahasiswa)
+
+# Menghapus salah satu data
+del mahasiswa["jurusan"]
+print("\nSetelah menghapus jurusan:")
+print(mahasiswa)
+
+# Menggunakan loop untuk menampilkan isi dictionary
+print("\nMenampilkan semua key dan value:")
+for key, value in mahasiswa.items():
+    print(f"{key} : {value}")
+
+# Mengecek apakah key ada dalam dictionary
+if "nama" in mahasiswa:
+    print("\nKey 'nama' ditemukan!")
+
+# Mendapatkan semua key
+print("\nSemua key:", list(mahasiswa.keys()))
+
+# Mendapatkan semua value
+print("Semua value:", list(mahasiswa.values()))
 
