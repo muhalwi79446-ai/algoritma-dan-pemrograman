d5af98b3d26727b72f9c0e629549611eec588290
muhalwi79446-ai <muh.alwi79446@gmail.com>
2025-12-11 12:29:50 +0800

    Update Tipe Data array satu Dimensi, Dua Dimensi dan Tiga Dimesni.py

--- final_test/Tipe Data array satu Dimensi, Dua Dimensi dan Tiga Dimesni.py
+++ final_test/Tipe Data array satu Dimensi, Dua Dimensi dan Tiga Dimesni.py
@@ -1,1 +1,35 @@
+# Array / List 1 Dimensi
+array1 = [10, 20, 30, 40, 50]
+
+print("Array 1 Dimensi:")
+print(array1)
+
+
+# Array / List 2 Dimensi (matriks)
+array2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("\nArray 2 Dimensi:")
+for row in array2:
+    print(row)
+
+
+# Array / List 3 Dimensi (kubus)
+array3 = [
+    [
+        [1, 2], 
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+]
+
+print("\nArray 3 Dimensi:")
+for layer in array3:
+    print(layer)
 
